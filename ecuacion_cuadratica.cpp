3c85f9c35be7bbcfe247f9abbca9c0dd9c3e1063
ronald <rchoquehuancar@unjbg.edu.pe>
2024-06-26 10:43:39 -0500

    modifique el codigo usando una funcion void y variables por valor y por referencia

--- ecuacion_cuadratica.cpp
+++ ecuacion_cuadratica.cpp
@@ -2,11 +2,27 @@
 #include <math.h>
 using namespace std;
 
-int main(void){
+void calcular_raices(float a, float b, float c, float &x1,float &x2,int &cant_soluciones){
+	float solucion;
+    solucion = (b*b) - (4*a*c); 
+    if(solucion<0){
+    	cant_soluciones=0;
+	}else if(solucion==0){
+    	x1 = (-b + sqrt(solucion)) / (2*a);
+    	cant_soluciones=1;
+	}else{
+		x1 = (-b + sqrt(solucion)) / (2*a);
+    	x2 = (-b - sqrt(solucion)) / (2*a);
+        cant_soluciones=2;
+	}
+}
+
+int main(){
 	// ax2 + bx + c = 0
 	
-    float a, b, c, x1, x2, solucion;
-    cout<<"Hallamos las raices de una ecuacion cuadratica con la forma:"<<endl<<"	ax2 + bx + c = 0"<< endl;
+    float a, b, c, x1, x2;
+    int cant_soluciones;
+    cout<<"Hallamos las raices de una ecuacion cuadratica con la forma:"<<endl<<"	ax2 + bx + c = 0"<< endl<<"donde (x2) siginifica (exis elevado al cuadrado)"<<endl;
 
     cout<<"Ingresa el valor de a: "<<endl;
     cin>>a;
@@ -16,22 +32,23 @@ int main(void){
     
     cout<<"Ingresa el valor de c: "<<endl;
     cin>>c;
+    calcular_raices(a,b,c,x1,x2,cant_soluciones);
     
-    
-    solucion = (b*b) - (4*a*c); 
-    if(solucion<0){
-    	cout<<"La ecuacion no tiene soluciones reales. ";
-	}else if(solucion==0){
-    	x1 = (-b + sqrt(solucion)) / (2*a);
-    	cout<<"la ecuacion tiene solo una solucion, es: "<<x1<<endl;
-	}else{
-		x1 = (-b + sqrt(solucion)) / (2*a);
-    	x2 = (-b - sqrt(solucion)) / (2*a);
-        cout<<"la ecuacion tiene dos soluciones: "<<endl;
+
+
+
+if(cant_soluciones==0){
+    	cout<<"La ecuacion"<<a<<"(x2) + "<<b<<"x + "<<c<<" = 0 "<<" no tiene soluciones reales. ";
+	}else if(cant_soluciones==1){
+    	cout<<"La ecuacion"<<a<<"(x2) + "<<b<<"x + "<<c<<" = 0 "<< " tiene solo una solucion, es: "<<x1<<endl;
+	}else if(cant_soluciones==2){
+        cout<<"La ecuacion"<<a<<"(x2) + "<<b<<"x + "<<c<<" = 0 "<< " tiene dos soluciones: "<<endl;
     	cout<<"x1 = "<<x1<<endl;
     	cout<<"x2 = "<<x2<<endl;
 	}
-    
-
+	
+	
+	
+	
     return 0;
 }
